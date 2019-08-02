0af7daf05c143459da9b9ce2676a7275026e41ec
Punith <punith.ch@gmail.com>
2019-08-01 21:40:10 -0600

    Fix: added dollar sign beside amount

--- src/containers/MainPage/itemField.js
+++ src/containers/MainPage/itemField.js
@@ -7,7 +7,7 @@ const ItemField = props => {
     return (
         <tr>
             <td>{props.data.itemName}</td>
-            <td>{props.data.itemCost}</td>
+            <td>${props.data.itemCost}</td>
             <td><i onClick={props.handleDelete} className="far fa-trash-alt"></i></td>
         </tr>
     )
